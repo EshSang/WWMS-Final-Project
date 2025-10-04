7c43e27213c6ff89a3367aab460e4e8ab98fcd4e
Eshana Sangeeth <eshanasangeeth11@gmail.com>
2025-10-04 12:19:23 +0530

    Complete login page and sign up page UI

--- src/Components/SignupForm.jsx
+++ src/Components/SignupForm.jsx
@@ -5,104 +5,151 @@ import Footer from "../Components/Footer"
 function SignupForm() {
   return (
     <div>
-     <div className="d-flex justify-content-center" style={{ marginTop: '120px', marginBottom: '145px' }}>
-        <div className="border rounded shadow p-20" style={{
-          width: '1030px',
-          height: '600px',
-          background: `linear-gradient(to right, white 0%, white 678px, #0FC5BB 678px, #0FC5BB 100%)`
-        }}>
+      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '88vh' }}>
+        <div className="shadow rounded" style={{ width: '1050px', background: '#fff' }}>
+          <div className="row g-0" style={{ height: '600px', display: 'flex', flexWrap: 'nowrap' }}>
+            {/* Left: Form */}
+            <div className="col-7 d-flex flex-column justify-content-center align-items-center" style={{ background: '#fff', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px', height: '600px' }}>
+              <div className="w-100 px-5">
+                <div className="text-center fs-3 mb-2" style={{ marginTop: '20px' }}>
+                  Create Account
+                </div>
+                <form>
+                  {/* First Name Input */}
+                  <div className="mb-2">
+                    <input
+                      type="fname"
+                      className="form-control"
+                      placeholder="First Name"
+                      style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}
+                    />
+                  </div>
+                  
+                  {/* Last Name Input */}
+                  <div className="mb-2">
+                    <input
+                      type="lname"
+                      className="form-control"
+                      placeholder="Last Name"
+                      style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}
+                    />
+                  </div>
 
-          <div className="text-center position-absolute fs-3" style={{  marginTop: '20px', marginLeft:'200px'}}>
-            Create Account
+                  {/* Phone Number Input */}
+                  <div className="mb-2">
+                    <input
+                      type="phonenumber"
+                      className="form-control"
+                      placeholder="Phone Number"
+                      style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}
+                    />
+                  </div>
+
+                  {/* Email Address Input */}
+                  <div className="mb-2">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email Address"
+                      style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}
+                    />
+                  </div>
+
+                  {/* Password Input */}
+                  <div className="mb-2">
+                    <div className="input-group" style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                      />
+                    </div>
+                  </div>
+                  {/* Forgot password */}
+                  <div className="d-flex justify-content-end" style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}>
+                    <small className="text-muted" style={{ cursor: "pointer" }}>
+                      Forgot Password?
+                    </small>
+                  </div>
+                  {/* Sign in button */}
+                  <div className="d-flex justify-content-center mt-4">
+                    <button
+                      type="submit"
+                      className="btn rounded-pill"
+                      style={{
+                        width: '300px',
+                        backgroundColor: '#0FC5BB',
+                        borderColor: '#0FC5BB',
+                        color: '#fff',
+                        fontWeight: 500
+                      }}
+                    >
+                      Sign Up
+                    </button>
+                  </div>
+                  {/* Divider */}
+                  <div className="d-flex align-items-center my-4" style={{ width: '100%', maxWidth: '400px', margin: '0 auto' }}>
+                    <hr className="flex-grow-1" />
+                    <span className="px-2 text-muted" style={{ fontSize: "12px" }}>Or</span>
+                    <hr className="flex-grow-1" />
+                  </div>
+                  {/* Google Sign up button */}
+                  <div className="d-flex justify-content-center mb-2">
+                    <button
+                      type="button"
+                      className="btn rounded-pill d-flex align-items-center justify-content-center"
+                      style={{
+                        width: '300px',
+                        backgroundColor: '#fff',
+                        color: '#0FC5BB',
+                        borderColor: '#0FC5BB',
+                        borderWidth: '2px',
+                        fontWeight: 500
+                      }}
+                    >
+                      <img
+                        src="https://img.icons8.com/color/24/000000/google-logo.png"
+                        alt="Google"
+                        className="me-2"
+                        style={{ width: '24px', height: '24px' }}
+                      />
+                      Sign Up with Google
+                    </button>
+                  </div>
+                  {/* Sign in link */}
+                  <div className="text-center mt-1" style={{ fontSize: '14px' }}>
+                    Already have an account?{' '}
+                    <button type="button" className="btn p-0" style={{ color: '#0FC5BB', fontWeight: 500, fontSize: '14px' }}>
+                      Sign In
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            {/* Right: Welcome */}
+            <div
+              className="col-5 d-flex flex-column justify-content-center align-items-center"
+              style={{
+                background: '#0FC5BB',
+                borderTopRightRadius: '10px',
+                borderBottomRightRadius: '10px',
+                color: '#fff',
+                height: '600px'
+              }}
+            >
+              <div className="text-center px-4 w-100">
+                <div className="h2 mb-3" style={{ fontWeight: 700 }}>Welcome folks</div>
+                <div style={{ fontSize: '18px', fontWeight: 400 }}>
+                  Enter your personal details<br />
+                  and start your journey with us
+                </div>
+              </div>
+            </div>
           </div>
-
-          {/* Login Form */}
-
-          <form style={{ marginTop: '100px', marginBottom: '80px' }}>
-            
-            {/* First Name Input box  */}
-            <div class="mb-3" style={{ marginLeft:'130px'}}>
-              <input type="fname" class="form-control" id="exampleInputFirstName" aria-describedby="fnameHelp" placeholder='First Name' style={{ width: '400px' }} />
-            </div>
-
-            {/* Last Name Input box  */}
-            <div class="mb-3" style={{ marginLeft:'130px'}}>
-              <input type="lname" class="form-control" id="exampleInputLastName" aria-describedby="lnameHelp" placeholder='Last Name' style={{ width: '400px' }} />
-            </div>
-
-            {/* Phone No Input box  */}
-            <div class="mb-3" style={{ marginLeft:'130px'}}>
-              <input type="phoneno" class="form-control" id="exampleInputPhoneNo" aria-describedby="phonenoHelp" placeholder='Phone No' style={{ width: '400px' }} />
-            </div>
-
-            {/* Email Input box  */}
-            <div class="mb-3" style={{ marginLeft:'130px'}}>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email address' style={{ width: '400px' }} />
-            </div>
-
-            {/* Password Input box  */}
-            <div class="mb-3" style={{ marginLeft:'130px'}}>
-              <input class="form-control" id="exampleInputPassword1" placeholder='Password' style={{ width: '400px' }} type="password"/>
-  
-              
-            </div>
-            {/* Sign up button  */}
-            <button type="submit" class="btn btn-primary rounded-pill position-absolute " style={{ marginLeft:'180px', marginTop: '20px', marginBottom: '50px', width: '300px', backgroundColor: '#0FC5BB', borderColor: '#0FC5BB' }}>Sign Up</button>
-
-            {/* Divider */}
-            <div className="d-flex align-items-center  my-3">
-              <hr className="flex-grow-0" style={{ marginLeft:'150px', marginTop: '80px', width: '150px' }} />
-              <span className="px-2 text-muted" style={{marginTop: '60px', fontSize: "12px"}}>Or</span>
-              <hr className="flex-grow-0" style={{ marginTop: '80px', width: '150px' }} />
-            </div>
-
-            {/* Google Sign up button  */}
-            <button type="submit" class="btn btn-primary rounded-pill position-absolute" style={{ marginLeft:'180px', marginTop: '1px', marginBottom: '30px', width: '300px', backgroundColor: '#FFFFFF', color: '#0FC5BB', borderColor: '#0FC5BB'}}>
-              <img
-                src="https://img.icons8.com/color/16/000000/google-logo.png"
-                alt="Google"
-                className="me-2 position-absolute" style={{left:'40px'}}
-            />Sign Up with Google</button>
-            
-          </form>
-
-          {/* Login Form Welcome folks section */}
-
-
-          <div className='mt-5 position-absolute '  style={{ marginTop: '100px',top: '250px',left: '1200px',height: '600px',width:'357px'}}>
-              <div className="h4 m-3 align-super text-light justify-content-center">Welcome folks</div>
-              <div className=" text-light mt-5 position-absolute text-wrap">Enter your personal details </div>  
-
-          </div> 
-
-          <div className='fs-10 p-20 position-absolute top-10 start-30 translate-middle'  style={{ left: '750px'}}>
-            Already have an account?
-            <button type="button" class="btn" style={{color:'#0FC5BB'}}>Sign In</button>
-          </div>
-
-          {/* please login your personal info */}
-
-          
-        {/* <div className="border rounded position-absolute"
-            style={{
-              top: '128px',
-              left: '1115px',
-              backgroundColor: '#0FC5BB',
-              width: '352px',
-              height: '600px',
-              borderTopRightRadius: '0.5rem',
-              borderBottomRightRadius: '0.5rem',
-              borderTopLeftRadius: '0',
-              borderBottomLeftRadius: '0'
-            }}>
-
-
-
-          </div> */}
-
         </div>
       </div>
       <Footer />
+
     </div>
   )
 }
--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,11 +1,13 @@
 
 import SigninForm from "../Components/SigninForm"
+import SignInNew from "../Components/SignInNew"
 
 
 function SignIn() {
   return (
     <>
         <SigninForm/>
+        {/* <SignInNew/> */}
     </>
     
   )
